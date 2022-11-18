a29117e529a869c0e0b5e62904f1df2fae367947
Inna <serebryakovainna@mail.ru>
2022-11-18 20:11:27 +0200

    Add commit 1

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ public class Main {
         String firstName = "Ivan";
         String middleName = "Ivanovich";
         String lastName = "Ivanov";
-        String fullName = firstName + " " + middleName + " " + lastName;
+        String fullName = lastName + " " + firstName + " " + middleName;
         System.out.println("Ф.И.О. сотрудника - " + fullName);
+    // Task 2 Ф.И.О. заглавными буквами
+        String capitalLetterName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + capitalLetterName);
     }
 }
