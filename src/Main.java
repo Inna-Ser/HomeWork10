28aff1c9b3fac3cc29e73f800aae1f56048c4e28
Inna <serebryakovainna@mail.ru>
2022-11-18 19:46:53 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    // Task 1 Данные сотрудников
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+    }
+}
